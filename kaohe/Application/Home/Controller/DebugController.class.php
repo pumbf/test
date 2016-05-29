8fcfdb046fdafa70d028805db28f6334368c36fa
wupeinan <892862286@qq.com>
2016-05-29 10:56:29 +0800

    test

--- kaohe/Application/Home/Controller/DebugController.class.php
+++ kaohe/Application/Home/Controller/DebugController.class.php
@@ -8,11 +8,11 @@ class DebugController //extends Controller
 {
 	//匹配信息
 	private $pattern = array(
-		'/<td[^>]*?>&nbsp;<\\/td>/',
-		'/<td[^>]*?>&nbsp;(?<time>(?<lesson>(?<classid>\\d*)<br>(?<classname>.*?)<br>(?<teacher>.*?)<br>(?<classroom>.*?)<br><font[^>]*?>(?<method>.*?)<\\/font><br>(?<weeks>.*?)<br>选课状态:(?<status>.*?)<font[^>]*?>(?<special>.*?)<\\/font><br>)*?)<\\/td>/',
-		'/(?<classid>\\d*)<br>(?<classname>.*?)<br>(?<teacher>.*?)<br>(?<classroom>.*?)<br><font[^>]*?>(?<method>.*?)<\\/font><br>(?<weeks>.*?)<br>选课状态:(?<status>.*?)<font[^>]*?>(?<special>.*?)<\\/font><br>/'
+		'/<td[^>]*?>.*?<\\/td>/',
+		'/(?<classid>\\d*)<br>(?<classname>.*?)<br>(?<teacher>.*?)<br>(?<classroom>.*?)<br><font[^>]*?>(?<method>.*?)<\\/font><br>(?<weeks>.*?)<br>选课状态:(?<status>.*?)<br><font[^>]*?>(?<special>.*?)<\\/font><br>/'
 
 	);
+
 	//过滤
 	private $annotation = array(
 		'/\\t/',
@@ -29,6 +29,7 @@ class DebugController //extends Controller
 	 */
 	public function __construct($startid, $num)
 	{
+		
 		for ($i = 0; $i < $num; $i++) {
 			$id = $startid + $i;
 			$this->runIt($id);
@@ -53,9 +54,14 @@ class DebugController //extends Controller
 			CURLOPT_URL             => $url,    //目标地址
 			CURLOPT_HEADER          => false, 	//是否显示请求头
 			CURLOPT_NOBODY          => false,	//显示<body>标签
+			CURLOPT_TIMEOUT			=> 60
 		);
 		curl_setopt_array($curl, $opt);
-		$text = curl_exec($curl);
+		try {
+			$text = curl_exec($curl);		
+		} catch (Exception $e) {
+			return NULL;
+		}
 		if(!$text){
 			return NULL;
 		}
@@ -67,40 +73,42 @@ class DebugController //extends Controller
 	private function analyze($text)
 	{
 		$model = M('Lesson');
+		//去注释
 		$text = preg_replace($this->annotation, '', $text);
+		//转码
 		$text = iconv('GBK', 'UTF-8', $text);
-		//file_put_contents('../../../../test/test.html', $text);
-		$message= array();
-		$text = preg_replace($this->pattern['0'], '<td height=58 width=11% align=center>&nbsp;0<br>null<br>null<br>null<br><font color=#ff0000>null</font><br>null<br>选课状态:null<br><font color=336699></font><br></td>', $text);
-		
-		preg_match_all($this->pattern['1'], $text, $subject);
-		file_put_contents('../../../../test/test.html', $subject[0][14]);
+		//把<td>内容单独拿出来
+		preg_match_all($this->pattern['0'], $text, $subject);
 		$lesson = array();
-		$time_lesson = array();
-		$n = 0;
-		//var_dump($subject[0][11]);
+		$lesson['studentid']  = $this->id;
 		foreach ($subject[0] as $key => $value) {
 			$week = ($key+1)%7;
 			$time = ($key+1)/7;
-			$num = preg_match_all($this->pattern['1'], $value, $message);
-			if ($message['classid']['0'] != '0') {
-
+			//分析数据
+			$num  = preg_match_all($this->pattern['1'], $value, $message);
+			if ($message['classid']['0']) {
+				//添加数据
 				for($i=0; $i<$num; $i++, $n++) {
-					$lesson['classid'] = $message['classid'][$i];
-					$lesson['teacher'] = $message['teacher'][$i];
+					$model 				 = 	M('Lesson');
+					$lesson['classid'] 	 = $message['classid'][$i];
+					$lesson['teacher'] 	 = $message['teacher'][$i];
 					$lesson['classname'] = $message['classname'][$i];
 					$lesson['classroom'] = $message['classroom'][$i];
-					$lesson['method'] = $message['method'][$i];
-					$lesson['weeks'] = $message['weeks'][$i];
-					$lesson['week'] = $message['week'][$i];
-					$lesson['status'] = $message['status'][$i];
-					$lesson['time']  = $time;
-					$lesson['week']	= $week;
-					$lesson['special']	= $message['special'][$i];
-					$lesson['studentid']  = $this->id;
-					//var_dump($lesson);
-					$model->create($lesson);
-					$model->add();
+					$lesson['method']    = $message['method'][$i];
+					$lesson['weeks'] 	 = $message['weeks'][$i];
+					$lesson['week'] 	 = $message['week'][$i];
+					$lesson['status'] 	 = $message['status'][$i];
+					$lesson['time']  	 = $time;
+					$lesson['week']		 = $week;
+					$lesson['special']	 = $message['special'][$i];
+					try{
+						$model->create($lesson);
+						$model->add();
+					} catch(Exception $e) {
+						continue;
+					}
+					unset($model);
+					sleep(0.1);
 				}
 				
 			}
--- kaohe/Application/Home/Controller/IndexController.class.php
+++ kaohe/Application/Home/Controller/IndexController.class.php
@@ -4,13 +4,23 @@ use Think\Controller;
 use Home\Controller\DebugController;
 class IndexController extends Controller {
     public function index($id = 2015210001){
-       $M = M('Lesson');
-       $data = $M->where('stdentid='.$id)->find();
-       $this->assign($data);
-       $this->display();
+    	try{
+       		$M = M('Lesson');
+       		$data = $M->where('stdentid='.$id)->find();
+       		$message;
+       		foreach ($data  as $value) {
+       		$message[$value['week']+7*$value['time']] = $data;
+       		}
+       		$this->assign($data);
+       		$this->display();
+       	} catch(Exception $e) {
+       		$this->error('数据为空，获取数据中....',U('Index/getData'));
+       	}
+       	
 
     }
     public function getData(){
+    	var_dump('获取数据中:)....');
     	 $Debug = new DebugController('2015210001',100);
     }
 }
